316d47530ac85b9df7cea3ebfeec65c241d64182
Harbingers <financesmacro@gmail.com>
2016-04-28 17:02:40 -0400

    Create scheduler.py

---
+++ scheduler.py
@@ -0,0 +1,10 @@
+# Reference：http://pages.cs.wisc.edu/~remzi/OSTEP/cpu-sched.pdf
+
+# 1. Scheduling Algorithms
+# 1) First In, Frist Out (FIFO)
+# 2) Shortest Job First (SJF)
+# 3) Shortest Time-to-Completion First (STCF)
+# 4) Round Robin
+
+# 2. This program, scheduler.py, allows you to see how different schedulers perform under
+# scheduling metrics such as response time, turnaround time, and total wait time
