e483191b10b643e3cce482c50395f65769483fb1
Harbingers <financesmacro@gmail.com>
2016-04-29 11:38:48 -0400

    Create segmentation.py

---
+++ segmentation.py
@@ -0,0 +1,11 @@
+# Reference: http://pages.cs.wisc.edu/~remzi/OSTEP/vm-segmentation.pdf
+
+# Test
+# python segmentation.py
+# python segmentation.py -h
+# python segmentation.py -s 100 -a 16 -p 32
+# python segmentation.py -a 1m -p 32m
+# python segmentation.py -a 128 -p 512 -b 0 -l 20 -B 512 -L 20 -s 0
+# python segmentation.py -a 128 -p 512 -b 0 -l 20 -B 512 -L 20 -s 1
+# python segmentation.py -a 128 -p 512 -b 0 -l 20 -B 512 -L 20 -s 2
+
